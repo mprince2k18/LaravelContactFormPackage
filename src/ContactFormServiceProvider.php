bbf2f7c7b91d0dc8110753fa990ec30cb6449776
mprince2k20 <mprince2k16@gmail.com>
2020-09-03 11:35:05 +0600

    first commit

---
+++ src/ContactFormServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+namespace MyVendor\contactform;
+
+use Illuminate\Support\ServiceProvider;
+
+class ContactFormServiceProvider extends ServiceProvider 
+{
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        $this->loadViewsFrom(__DIR__.'/resources/views', 'contactform');
+    }
+    
+    public function register()
+    {
+        //
+    }
+
+}
